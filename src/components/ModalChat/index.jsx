b59e5e0b1381b82c91916d2888364dfb2a464a8b
Thiago Koch <tkoch.dev@gmail.com>
2023-09-14 00:08:04 -0300

    feat: add functionality on ModalChat

--- src/components/ModalChat/index.jsx
+++ src/components/ModalChat/index.jsx
@@ -1,24 +1,92 @@
 import './style.scss'
-import React, { useState, useEffect } from 'react';
+import { Input } from '../Input';
+import { TbSend } from 'react-icons/tb';
 import { BsChatLeft } from 'react-icons/bs';
 import { IoIosArrowDown } from 'react-icons/io';
-import { TbSend } from 'react-icons/tb';
+import React, { useState, useEffect } from 'react';
 import { PiArrowElbowRightUpBold } from 'react-icons/pi';
 import { FaCompressAlt, FaExpandAlt } from 'react-icons/fa';
 import chatAvatarMenu from '../../assets/chatAvatarMenu.svg';
-import { Input } from '../Input';
+import chatAvatarMessage from '../../assets/chatAvatarMessage.svg';
+import { HandleSendMsgByChat } from '../../functions/HandleSendMsgByChat';
 
 export function ModalChat() {
   const chatPanelMainRef = React.useRef(null);
   const chatPanelRef = React.useRef(null);
+  const historicRef = React.useRef(null);
+  const inputRef = React.useRef(null);
+  const mainTextRef = React.useRef(null)
+
   const [isOpened, setIsOpened] = useState(false);
   const [isSmall, setIsSmall] = useState(true);
+  const [inputMessage, setInputMessage] = useState('');
+  const [serverResponse, setServerResponse] = useState('');
 
   useEffect(() => {
     chatPanelRef.current = document.querySelector(".modalChatStructure__chatPanel");
     chatPanelMainRef.current = document.querySelector('.modalChatStructure__main');
-  })
+    historicRef.current = document.querySelector('.modalChatStructure__historic');
+    inputRef.current = document.querySelector('.inputStructure--chat');
+    mainTextRef.current = document.querySelector('.modalChatStructure__mainText');
+  }, []);
 
+  useEffect(() => {
+    if(serverResponse !== '') {
+      showServerMsg();
+
+      if(chatPanelMainRef.current) {
+        chatPanelMainRef.current.scrollTop = chatPanelMainRef.current.scrollHeight;
+      }
+    }
+
+  }, [serverResponse]);
+
+
+  const updateMainTextVisibility = () => {
+    if(historicRef.current && historicRef.current.children.length > 0) {
+      mainTextRef.current.classList.add('modalChatStructure__mainText--hidden');
+    } else {
+      mainTextRef.current.classList.add('modalChatStructure__mainText--hidden');
+    }
+  }
+
+  const showServerMsg = () => {
+
+    const children = Array.from(historicRef.current.children)
+    const lastChild = children[children.length -1];
+
+    if(lastChild && lastChild.classList.contains('modalChatStructure__userMsgBox')) {
+      var boxMsg = document.createElement('div')
+      boxMsg.className = 'modalChatStructure__serverMsgBox'
+      
+      var serverAvatar = document.createElement('img')
+      serverAvatar.className = 'modalChatStructure__serverMsgAvatar'
+      serverAvatar.src = chatAvatarMessage
+      
+      var serverMsg = document.createElement('p')
+      serverMsg.className = 'modalChatStructure__serverMsgText'
+      serverMsg.innerHTML = serverResponse
+      
+      boxMsg.appendChild(serverAvatar)
+      boxMsg.appendChild(serverMsg)
+      historicRef.current.appendChild(boxMsg);
+    }
+  }
+
+  const showUserMsg = () => {
+    var boxMsg = document.createElement('div')
+    boxMsg.className = 'modalChatStructure__userMsgBox'
+
+    var userMsg = document.createElement('p')
+    userMsg.className = 'modalChatStructure__userMsgText'
+    userMsg.innerHTML = inputMessage
+
+    boxMsg.appendChild(userMsg)
+    historicRef.current.appendChild(boxMsg);
+    inputRef.current.value = '';
+    setInputMessage(inputRef.current.value)
+    updateMainTextVisibility();
+  }
 
   const toggleChat = () => {
     setIsOpened(!isOpened);
@@ -72,24 +140,41 @@ export function ModalChat() {
         </div>
 
         <div className='modalChatStructure__main'>
-          <p className='modalChatStructure__main__text'>
-            Role para cima
-          </p>
 
-          <PiArrowElbowRightUpBold/>
-          <p className='modalChatStructure__main__text'>
-            para ver o histórico
-          </p>
+          <div className='modalChatStructure__mainText'>
+            <p className='modalChatStructure__mainText__item'>
+              Role para cima
+            </p>
+            <PiArrowElbowRightUpBold/>
+            <p className='modalChatStructure__mainText__item'>
+              para ver o histórico
+            </p>
+          </div>
+
+          <div className='modalChatStructure__historic'>
+
+          </div>
+
+          
         </div>
 
-        <div className='modalChatStructure__input'>
+          <div className='modalChatStructure__input'>
 
-          <Input className='inputStructure--chat' placeholder='Digite sua dúvida'/>
-          <button className='modalChatStructure__sendButton'>
-            <TbSend/>
-          </button>
+            <Input 
+              className='inputStructure--chat' 
+              placeholder='Digite sua dúvida'
+              onChange={e => setInputMessage(e.target.value)}
+            />
+            <button className='modalChatStructure__sendButton' onClick={ async () => {
+              const response = await HandleSendMsgByChat({inputMessage});
+              showUserMsg();
+              setServerResponse(response);
+              }}>
+              <TbSend/>
+            </button>
 
-        </div>
+          </div>
+
       </div>
 
       <div className='modalChatStructure__buttonSpace'>
@@ -100,4 +185,4 @@ export function ModalChat() {
     </div>
     </>
   )
-}''
+}
